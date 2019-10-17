33a42d36e18e21458878a5d294dd85ef3279eb3e
Robert Evers <robert.evers9@gmail.com>
2019-10-17 07:19:10 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,6 @@ app.get('/savings', (req, res) => {res.render('account', {account: accounts.savi
 app.get('/checking', (req, res) => {res.render('account', {account: accounts.checking})});
 app.get('/credit', (req, res) => {res.render('account', {account: accounts.credit})});
 
-app.get('/profile' (req, res) => {res.render('profile', { user: users[0] })});
+app.get('/profile', (req, res) => {res.render('profile', { user: users[0] })});
 
 app.listen(3000, () => console.log('PS Project Running on port 3000'));
